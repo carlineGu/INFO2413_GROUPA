f1f57d32ef9f5a88a53200de590fe3ed303bcb5c
Varleen <varleenjaswal@gmail.com>
2026-08-09 15:23:58 -0700

    Manage Users page created with HTML, CSS, and JS

---
+++ frontend/js/admin_man_users.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", loadUsers);
+
+async function loadUsers() {
+  try {
+    const users =
+      await CampusMarketplace.request("/user/admin");
+      console.log("Users:", users);
+    
+
+    const tbody =
+      document.getElementById("usersBody");
+
+    tbody.innerHTML = "";
+
+    users.forEach((user) => {
+
+      const actionButton =
+        user.account_status === "SUSPENDED"
+          ? `<button onclick="reinstateUser(${user.user_id})">Reinstate</button>`
+          : `<button onclick="suspendUser(${user.user_id})">Suspend</button>`;
+
+      tbody.innerHTML += `
+        <tr>
+          <td>
+            ${CampusMarketplace.escapeHtml(
+              user.first_name + " " + user.last_name
+            )}<br>
+            ${CampusMarketplace.escapeHtml(user.email_addr)}
+          </td>
+
+          <td>${user.listing_count}</td>
+
+          <td>${CampusMarketplace.escapeHtml(
+            user.account_status
+          )}</td>
+
+          <td>
+            ${actionButton}
+
+            <button
+              class="remove-btn"
+              onclick="removeUser(${user.user_id})">
+              Remove
+            </button>
+          </td>
+        </tr>
+      `;
+    });
+
+
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
