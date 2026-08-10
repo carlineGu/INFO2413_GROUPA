574220721a7681e30537ad2a8a1d2f931544419f
Varleen <varleenjaswal@gmail.com>
2026-08-10 11:52:38 -0700

    CMA Database Fix

---
+++ CMA-2314.session.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (user_id, first_name, last_name, user_role, email_addr, password_hash,account_status, created_at) 
+VALUES (4, 'admin', '', 'ADMIN', 'admin', '$2b$10$o3FOIDAKA001Wmtm9xf2feg.NfggGpyXjS3IYh3SuBx1W2qv1SRwS', 'ACTIVE', NOW());
